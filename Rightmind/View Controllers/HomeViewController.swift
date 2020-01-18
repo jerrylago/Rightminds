522f0e2e62ed65dec37a619be523472c00a0fb3e
Jerry LaGore <jerrylagore@Jerrys-MacBook-Pro.local>
2020-01-18 17:02:01 -0500

    Finished unpolished home screen, working on quiz

--- Rightmind/View Controllers/HomeViewController.swift
+++ Rightmind/View Controllers/HomeViewController.swift
@@ -32,6 +32,71 @@ class HomeViewController: UIViewController {
         
         title = "Rightminds"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setUpLabels(label: welcomeLabel, title: "Welcome!", size: 36)
+        setUpLabels(label: videoLabel, title: "Video of the Day!", size: 24)
+        setUpLabels(label: descriptionLabel, title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget porta metus. Praesent eu urna libero.", size: 24)
+        
+        video.image = UIImage(named: "AlzheimersDay-Banner")!
+        
+        quizButton.setTitle("Begin Quiz", for: .normal)
+        quizButton.addTarget(self, action: #selector(quizTapped), for: .touchUpInside)
+        quizButton.layer.cornerRadius = 10
+        quizButton.backgroundColor = .blue
+        quizButton.setTitleColor(.white, for: .normal)
+        
+        setConstraints()
+    }
+    
+    func setUpLabels(label: UILabel, title: String, size: CGFloat) {
+        label.text = title
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 5
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: size)
+    }
+    
+    func setConstraints() {
+        
+        welcomeLabel.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(170)
+            make.width.equalTo(200)
+            make.height.equalTo(70)
+        }
+        
+        videoLabel.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(self.welcomeLabel).offset(80)
+            make.width.equalTo(250)
+            make.height.equalTo(70)
+        }
+        
+        video.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(self.videoLabel).offset(70)
+            make.width.equalTo(270)
+            make.height.equalTo(270)
+        }
+        
+        descriptionLabel.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.top.equalTo(self.video).offset(280)
+            make.width.equalTo(300)
+            make.height.equalTo(100)
+        }
+        
+        quizButton.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.bottom.equalTo(-100)
+            make.width.equalTo(150)
+            make.height.equalTo(60)
+        }
+    }
+    
+    @objc func quizTapped() {
+        navigationController?.pushViewController(QuizViewController(), animated: true)
     }
 
 }
--- Rightmind/View Controllers/QuizViewController.swift
+++ Rightmind/View Controllers/QuizViewController.swift
@@ -31,62 +31,79 @@ class QuizViewController: UIViewController {
         view.addSubview(answer2Button)
         view.addSubview(answer3Button)
         view.addSubview(answer4Button)
-        
+        view.backgroundColor = .white
         answer1Button.addTarget(self, action: #selector(answer1Tapped), for: .touchUpInside)
         answer2Button.addTarget(self, action: #selector(answer2Tapped), for: .touchUpInside)
         answer3Button.addTarget(self, action: #selector(answer3Tapped), for: .touchUpInside)
         answer4Button.addTarget(self, action: #selector(answer4Tapped), for: .touchUpInside)
+        setUpLabels(label: questionLabel, title: "Where was the Hindenburg trying to land when it was destroyed?", size: 30)
         
         setConstraints()
         
     }
     
+    func setUpLabels(label: UILabel, title: String, size: CGFloat) {
+        label.text = title
+        label.textColor = .black
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 15
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: size)
+    }
+    
     func setConstraints() {
         
-        answer1Button.snp.makeConstraints { (make) in
+        questionLabel.snp.makeConstraints { (make) in
             make.centerX.equalTo(self.view)
-            make.centerY.equalTo(self.view).offset(100)
-            make.width.equalTo(200)
-            make.height.equalTo(60)
+            make.centerY.equalTo(self.view).offset(-30)
+            make.width.equalTo(345)
+            make.height.equalTo(200)
+        }
+        
+        answer1Button.snp.makeConstraints { (make) in
+            make.left.equalTo(40)
+            make.width.equalTo(150)
+            make.height.equalTo(80)
+            make.bottom.equalTo(self.answer3Button).offset(-120)
         }
         
         answer2Button.snp.makeConstraints { (make) in
-            make.centerX.equalTo(self.view)
-            make.centerY.equalTo(self.view).offset(200)
-            make.width.equalTo(200)
-            make.height.equalTo(60)
+            make.right.equalTo(-40)
+            make.width.equalTo(150)
+            make.height.equalTo(80)
+            make.bottom.equalTo(self.answer4Button).offset(-120)
         }
         
         answer3Button.snp.makeConstraints { (make) in
-            make.centerX.equalTo(self.view)
-            make.centerY.equalTo(self.view).offset(300)
-            make.width.equalTo(200)
-            make.height.equalTo(60)
+            make.left.equalTo(40)
+            make.width.equalTo(150)
+            make.height.equalTo(80)
+            make.bottom.equalTo(-150)
         }
         
         answer4Button.snp.makeConstraints { (make) in
-            make.centerX.equalTo(self.view)
-            make.centerY.equalTo(self.view).offset(400)
-            make.width.equalTo(200)
-            make.height.equalTo(60)
+            make.right.equalTo(-40)
+            make.width.equalTo(150)
+            make.height.equalTo(80)
+            make.bottom.equalTo(-150)
         }
         
     }
 
     @objc func answer1Tapped() {
-        
+        print("Answer 1")
     }
     
     @objc func answer2Tapped() {
-        
+        print("Answer 2")
     }
     
     @objc func answer3Tapped() {
-        
+        print("Answer 3")
     }
     
     @objc func answer4Tapped() {
-        
+        print("Answer 4")
     }
     
 }
